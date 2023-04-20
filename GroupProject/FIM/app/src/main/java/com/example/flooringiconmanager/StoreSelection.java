8547175a5db90a0aacc38eae811047e5ca922b29
Jonnathan Saavedra <jonnjstart@gmail.com>
2023-04-19 23:22:30 -0400

    added store selection class and its activity xml

---
+++ GroupProject/FIM/app/src/main/java/com/example/flooringiconmanager/StoreSelection.java
@@ -0,0 +1,28 @@
+package com.example.flooringiconmanager;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class StoreSelection extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_store_selection);
+        List<String> storeNamesList = new ArrayList<>();
+        storeNamesList.add("Store 1");
+        storeNamesList.add("Store 2");
+        storeNamesList.add("Store 3");
+
+        Spinner spinner = findViewById(R.id.spinner);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, storeNamesList);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+    }
+
+}
