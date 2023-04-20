3310e07202b1ceae44b8801661974fdb64d31822
VRS-Empty <83312074+VRS-Empty@users.noreply.github.com>
2023-04-19 22:49:48 -0400

    Add files via upload

---
+++ GroupProject/FIM/app/src/main/java/com/example/flooringiconmanager/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.flooringiconmanager;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button userLogin, employeeLogin;
+
+    private EditText emplyeeId;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        userLogin = findViewById(R.id.user);
+        employeeLogin = findViewById(R.id.employee);
+        emplyeeId = findViewById(R.id.employeeId);
+
+        userLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, UserHomeActivity.class);
+                intent.putExtra("employ", 0);
+                startActivity(intent);
+            }
+        });
+
+        employeeLogin.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                LoginEmployee();
+            }
+        });
+
+        private void LoginEmployee(){
+            String id = employeeId.getText().toString();
+
+        }
+    }
+}
